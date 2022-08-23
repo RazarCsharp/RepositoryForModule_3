b6bb1397dc88bdd4336eb7a80558d8c671d98468
Name <email@example.ru>
2022-08-23 19:08:13 +0300

    добавлена: Программа для задания 3.3.2

--- ConsoleAppForModule_3/Program.cs
+++ ConsoleAppForModule_3/Program.cs
@@ -6,9 +6,28 @@ namespace ConsoleAppForModule_3
     {
         static void Main(string[] args)
         {
-            string myName = "Jon";
+            const string myName = "\n \n \n \t \t \t \t Wake up Neo ... \n \t \t \t \t The matrix has you ...";
+
             
             Console.WriteLine(myName);
+            Console.WriteLine("\t \t Привет,\n \t \t мир!");
+            Console.WriteLine("\u0023");
+            Console.WriteLine("\x01");
+            Console.WriteLine(0x0A);
+            Console.WriteLine(0b11);
+            Console.WriteLine(5.5);
+
+            // Программа для задания 3.3.2
+            string MyName = "Jane";
+            byte MyAge = 27;
+            bool HaveIApet = true;
+            double MyShoeSize = 37.5;
+
+            Console.WriteLine("\n My name is " + MyName + "\n My Age " + MyAge+ "\n Do I have a pet? " + HaveIApet + "\n My shoe size is " + MyShoeSize);
+
+
+            
+
 
             Console.ReadKey();
         }
