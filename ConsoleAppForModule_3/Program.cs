e6efb35a3d7b7c2b0c8ee83eb9c6d50a3038ffbb
Name <email@example.ru>
2022-08-23 16:02:28 +0300

    Добавьте файлы проекта.

---
+++ ConsoleAppForModule_3/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+namespace ConsoleAppForModule_3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string myName;
+            myName = "Jon";
+
+            Console.WriteLine(myName);
+
+            Console.ReadKey();
+        }
+    }
+}
